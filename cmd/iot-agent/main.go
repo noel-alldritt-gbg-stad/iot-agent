214d088cdd81e4984be61a1902ec489d824189be
Isak Styf <isak.styf@masarinconsulting.se>
2022-03-17 21:37:09 +0100

    Non refactored code to connect to a mqtt broker and pront out the payloads

--- cmd/iot-agent/main.go
+++ cmd/iot-agent/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"crypto/tls"
+	"fmt"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/uuid"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	options := mqtt.NewClientOptions()
+	// broker IP and port
+
+	connectionString := fmt.Sprintf("tls://%s:8883", os.Getenv("MQTT_HOST"))
+	options.AddBroker(connectionString)
+
+	options.Username = os.Getenv("MQTT_USER")
+	options.Password = os.Getenv("MQTT_PASSWORD")
+
+	options.SetClientID("diwise/iot-agent" + uuid.NewString())
+	options.SetDefaultPublishHandler(MessageHandler)
+
+	options.OnConnect = func(c mqtt.Client) {
+		fmt.Println("connected!")
+		c.Subscribe("application/53/device/#", 0, nil)
+	}
+
+	options.OnConnectionLost = func(c mqtt.Client, err error) {
+		panic(fmt.Sprintf("connection lost: %s\n", err.Error()))
+	}
+
+	options.TLSConfig = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	client := mqtt.NewClient(options)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func MessageHandler(client mqtt.Client, msg mqtt.Message) {
+	payload := msg.Payload()
+	fmt.Printf("received payload %s", string(payload))
+	msg.Ack()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/diwise/iot-agent
 
 go 1.17
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.3.5
+	github.com/google/uuid v1.3.0
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+)
